8ed184561f638a804ae25933150449a1fa88c4b3
Steph <1000p@mail.ru>
2022-02-03 07:18:19 +0700

    начал 14 задачу, нужны идеи

---
+++ 14. Найти третью цифру числа или ее нет/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+WriteLine("Введи любое число, и я напишу третью цифру в ней: ");
+
+int n = int.Parse(ReadLine());
+
+int t = 1000
+
+
+if ( n / 100 < 1){
+    WriteLine("Тут нет третьего числа");
+}
+
+ else if (n < 1000){
+     WriteLine( n % 10 );
+ }
+
+
+
+
+ //else if (n < 10000){
+ //    WriteLine(( n % 10 - n % 1 ) / 1);
+ //}
+// Надо придумать формулу
